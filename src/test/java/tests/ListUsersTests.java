5ba67c9e4ea8872ed801f87060252f9ece7a8468
Ghost-of-Mars <zzz9510@mail.ru>
2024-01-27 19:26:51 +0300

    new test

--- src/test/java/tests/ListUsersTests.java
+++ src/test/java/tests/ListUsersTests.java
@@ -92,5 +92,32 @@ public class ListUsersTests extends BaseTest {
         });
     }
 
+    @Disabled("Так надо")
+    @Test
+    void listUsers4Equal1() {
+
+        ResponseListModel responseListModel = step("Отправка запроса и его маппинг на модель", () ->
+                given()
+                        .filter(new AllureRestAssured())
+                        .formParam("page", 1)
+                        .formParam("per_page", 2)
+                        .when()
+                        .get("/users")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(ResponseListModel.class));
+
+        step("Сравнение ", () -> {
+            assertThat(responseListModel.getData()[0].getId()).isEqualTo(1);
+            assertThat(responseListModel.getData()[0].getEmail()).isEqualTo("george.bluth@reqres.in");
+            assertThat(responseListModel.getData()[0].getFirst_name()).isEqualTo("George");
+            assertThat(responseListModel.getData()[0].getLast_name()).isEqualTo("Bluth");
+            assertThat(responseListModel.getData()[0].getAvatar()).isEqualTo("https://reqres.in/img/faces/1-image.jpg");
+
+        });
+    }
+
 
 }
